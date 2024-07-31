d99552c0771d48ffd0ee69a4d2da8e608cdb8dcb
Firuddinm <firuddinmustafayev@gmail.com>
2024-07-31 23:30:46 +0400

    first commit

---
+++ wps.js
@@ -0,0 +1,54 @@
+let BaseUrl = "http://localhost:5050/data";
+let cardsDiv = document.querySelector(".products-container");
+let addForm = document.querySelector(".add-form");
+let nameInp = document.querySelector(".nameInp");
+let descInp = document.querySelector(".descInp");
+
+async function GetDatas() {
+    try {
+        let datas = await axios.get(BaseUrl);
+        CreateCards(datas.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function CreateCards(cards) {
+    cardsDiv.innerHTML = "";
+    cards.forEach(element => {
+        cardsDiv.innerHTML += `
+            <div class="cardd">
+                <div class="card-top">
+                    <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" class="image">
+                </div>
+                <div class="card-bottom">
+                    <h3>${element.name}</h3>
+                    <div>${element.description}</div>
+                    <button class="del-btn" btn-id=${element._id}>Delete</button>
+                </div>
+            </div>
+        `;
+        let delBtns = document.querySelectorAll(".del-btn");
+        delBtns.forEach(btn => {
+            btn.addEventListener("click", async function() {
+                let id = btn.getAttribute("btn-id");
+                await axios.delete(`${BaseUrl}/${id}`);
+                GetDatas();
+            });
+        });
+    });
+}
+
+addForm.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    let newPro = {
+        name: nameInp.value,
+        description: descInp.value
+    };
+    await axios.post(`${BaseUrl}`, newPro);
+    nameInp.value = "";
+    descInp.value = "";
+    GetDatas();
+});
+
+GetDatas();
